2dafbf536396d3573d77f26e84c438f50f9c0e69
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-10-02 15:41:28 +0200

    🚧 Work in progress

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
 const State = enum {
     none,
@@ -7,18 +8,48 @@ const State = enum {
     black,
 };
 
+const Error = error{ ParseMoveError, InvalidMoveError };
+
+const XY = struct {
+    x: u8,
+    y: u8,
+};
+
 const Board = struct {
-    cells: [64]State,
+    cells: [64]State = [_]State{.none} ** 64,
 
     fn init() Board {
-        var board = Board{
-            .cells = [_]State{.none} ** 64,
-        };
+        var board = Board{};
         board.cells[27] = .white;
         board.cells[28] = .black;
         board.cells[35] = .black;
         board.cells[36] = .white;
         return board;
+    }
+
+    fn get(self: Board, x: u8, y: u8) State {
+        return self.cells[y * 8 + x];
+    }
+
+    fn set(self: *Board, x: u8, y: u8, state: State) !void {
+        var board = Board{};
+        _ = board;
+        if (self.get(x, y) != .none) {
+            return error.InvalidMoveError;
+        }
+        self.cells[y * 8 + x] = state;
+    }
+
+    fn parseMove(move: []u8) !XY {
+        if (move.len != 2) {
+            return error.ParseMoveError;
+        }
+        const x = move[0] - 'a';
+        const y = move[1] - '1';
+        if (x > 7 or y > 7) {
+            return error.ParseMoveError;
+        }
+        return XY{ .x = x, .y = y };
     }
 
     fn display(self: Board) !void {
@@ -31,8 +62,8 @@ const Board = struct {
                 const index = y * 8 + x;
                 switch (self.cells[index]) {
                     .none => try stdout.print("{s}", .{"- "}),
-                    .white => try stdout.print("{s}", .{"O "}),
-                    .black => try stdout.print("{s}", .{"X "}),
+                    .white => try stdout.print("{s}", .{"○ "}),
+                    .black => try stdout.print("{s}", .{"● "}),
                 }
             }
             try stdout.print("{d}\n", .{x + 1});
@@ -42,6 +73,25 @@ const Board = struct {
 };
 
 pub fn main() !void {
-    const board = Board.init();
-    try board.display();
+    var board = Board.init();
+    var buffer: [1024]u8 = undefined;
+
+    while (true) {
+        try board.display();
+        try stdout.print("\n", .{});
+        try stdout.print("Turn: ●\n", .{});
+        try stdout.print("\n", .{});
+        try stdout.print("Enter move: ", .{});
+
+        var xy: ?XY = null;
+        while (xy == null) {
+            const move = try stdin.readUntilDelimiter(&buffer, '\n');
+            xy = Board.parseMove(move) catch {
+                try stdout.print("Invalid move, try again: ", .{});
+                continue;
+            };
+        }
+        try board.set(xy.?.x, xy.?.y, .black);
+        try stdout.print("\n", .{});
+    }
 }
