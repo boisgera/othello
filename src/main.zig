f179de198ef5faaa05db6207d029d14c6d4cbb72
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-10-05 16:49:44 +0200

    🚧 Work in progress

--- src/main.zig
+++ src/main.zig
@@ -16,12 +16,12 @@ const XY = struct {
 
 const Board = struct {
     cells: [64]?State = .{null} ** 64,
-    turn: ?State = null,
+    turn: State = undefined,
 
     fn init() Board {
         var self = Board{};
-        self.set(3, 3, .white);
-        self.set(4, 3, .black);
+        self.set(3, 3, .black);
+        self.set(4, 3, .white);
         self.set(3, 4, .white);
         self.set(4, 4, .black);
         self.turn = .black;
@@ -29,14 +29,48 @@ const Board = struct {
     }
 
     fn get(self: Board, x: u8, y: u8) ?State {
-        return self.cells[y * 8 + x];
+        return self.cells[x + y * 8];
     }
 
     fn set(self: *Board, x: u8, y: u8, state: ?State) void {
-        self.cells[y * 8 + x] = state;
+        self.cells[x + y * 8] = state;
     }
 
-    fn parseMove(move: []u8) !XY {
+    fn flip(self: *Board, x: u8, y: u8, state: State) !std.ArrayList(XY) {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+        var results = std.ArrayList(XY).init(allocator);
+        if (self.get(x, y)) |_| {
+            return results;
+        }
+        try results.append(.{ x, y }); // Oops, we have to remove it if we fail
+        const directions: [8]struct { i8, i8 } = .{ .{ 0, -1 }, .{ 1, -1 }, .{ 1, 0 }, .{ 1, 1 }, .{ 0, 1 }, .{ -1, 1 }, .{ -1, 0 }, .{ -1, -1 } };
+        for (directions) |d| {
+            var current = .{ @as(i16, x), @as(i16, y) };
+            var candidates = std.ArrayList(struct { u8, u8 }).init(allocator);
+            while (true) {
+                current[0] = current[0] + @as(i16, d[0]);
+                current[1] = current[1] + @as(i16, d[1]);
+                if (current[0] < 0 or current[0] > 7 or current[1] < 0 or current[1] > 7) {
+                    break;
+                }
+                if (self.get(@intCast(current[0]), @intCast(current[1]))) |s| {
+                    if (s != state) {
+                        try candidates.append(.{ @intCast(current[0]), @intCast(current[1]) });
+                        continue;
+                    } else {
+                        try results.appendSlice(candidates.items);
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return results;
+    }
+
+    fn parseMove(move: []const u8) !XY {
         if (move.len != 2) {
             return error.ParseMoveError;
         }
@@ -50,21 +84,21 @@ const Board = struct {
 
     fn display(self: Board) !void {
         try stdout.print("{s}", .{"  a b c d e f g h\n"});
-        var x: u8 = 0;
-        while (x < 8) : (x += 1) {
-            try stdout.print("{d} ", .{x + 1});
-            var y: u8 = 0;
-            while (y < 8) : (y += 1) {
+        var y: u8 = 0;
+        while (y < 8) : (y += 1) {
+            try stdout.print("{d} ", .{y + 1});
+            var x: u8 = 0;
+            while (x < 8) : (x += 1) {
                 if (self.get(x, y)) |state| {
                     switch (state) {
                         .white => try stdout.print("{s}", .{"○ "}),
                         .black => try stdout.print("{s}", .{"● "}),
                     }
                 } else {
-                    try stdout.print("{s}", .{"- "});
+                    try stdout.print("{s}", .{"· "});
                 }
             }
-            try stdout.print("{d}\n", .{x + 1});
+            try stdout.print("{d}\n", .{y + 1});
         }
         try stdout.print("{s}", .{"  a b c d e f g h\n"});
     }
@@ -72,9 +106,7 @@ const Board = struct {
     fn promptPlay(self: Board) !void {
         try self.display();
         try stdout.print("\n", .{});
-        if (self.turn) |state| {
-            try stdout.print("Turn: {s}\n\n", .{@tagName(state)});
-        }
+        try stdout.print("Turn: {s}\n\n", .{@tagName(self.turn)});
         try stdout.print("Enter move: ", .{});
     }
 };
@@ -95,12 +127,22 @@ pub fn main() !void {
                 try stdout.print("Invalid move, try again: ", .{});
                 continue;
             };
-            if (board.get(xy[0], xy[1])) |_| {
+            const flips = try board.flip(xy[0], xy[1], board.turn);
+            if (flips.items.len == 0) {
                 try stdout.print("Invalid move, try again: ", .{});
                 continue;
+            } else {
+                board.set(xy[0], xy[1], board.turn);
+                for (flips.items) |xy_| {
+                    board.set(xy_[0], xy_[1], board.turn);
+                }
+                if (board.turn == .black) {
+                    board.turn = .white;
+                } else {
+                    board.turn = .black;
+                }
+                try board.promptPlay();
             }
-            board.set(xy[0], xy[1], .black);
-            try board.promptPlay();
         }
     }
 }
@@ -111,4 +153,12 @@ test "Board data not shared" {
     board_2.set(0, 0, .black);
     try expect(board_1.get(0, 0) == null);
     try expect(&board_1.cells != &board_2.cells);
+}
+
+test "Parse move" {
+    try expect(std.meta.eql(try Board.parseMove("a1"), .{ 0, 0 }));
+    try expect(std.meta.eql(try Board.parseMove("a2"), .{ 0, 1 }));
+    try expect(std.meta.eql(try Board.parseMove("a8"), .{ 0, 7 }));
+    try expect(std.meta.eql(try Board.parseMove("h1"), .{ 7, 0 }));
+    try expect(std.meta.eql(try Board.parseMove("h8"), .{ 7, 7 }));
 }
